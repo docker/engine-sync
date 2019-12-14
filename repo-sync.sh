0bfa9eafbd940f33318e3323cbda5d357ba9889a
Brian Goff <cpuguy83@gmail.com>
2019-12-14 06:51:30 -0800

    Add repo-sync script
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

---
+++ repo-sync.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e -u -o pipefail
+
+export ORIGIN="${1}"
+export FORK="${2}"
+
+dir=$(mktemp -d)
+
+(
+	cd "${dir}"
+	git init --bare .
+
+	git remote add origin "${ORIGIN}"
+	git remote add fork "${FORK}"
+
+	BRANCHES=($(git ls-remote --heads origin | awk '{ print $2 }' | awk -F'/' '{ print $3 }'))
+
+	BRANCHES_LOCAL=()
+	for b in ${BRANCHES[@]}; do
+		BRANCHES_LOCAL+=("${b}:${b}")
+	done
+
+	git fetch --update-head-ok origin ${BRANCHES_LOCAL[@]}
+	git push fork ${BRANCHES[@]}
+
+	git fetch --tags origin
+	git push --tags fork
+)
+
+rm -rf "${dir}"
